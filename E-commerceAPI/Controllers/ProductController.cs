acf317a566c6de4dca288c261b8299a0dd1784a3
VullnetLipovica <vl57425@ubt-uni.net>
2024-01-01 16:34:32 +0100

    Add project files.

---
+++ E-commerceAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using E_commerceAPI.Data;
+using E_commerceAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ E-commerceAPI/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using E_commerceAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerceAPI.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
