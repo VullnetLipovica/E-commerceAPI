72203d228605e6eb21658681bc75c1ce55b0e64a
ErzaBoshnjaku <erzaboshnjaku28@gmail.com>
2024-01-03 16:27:02 +0100

    Routing,details view for each product,navbar

--- client/src/app/features/catalog/ProductCard.tsx
+++ client/src/app/features/catalog/ProductCard.tsx
@@ -1,11 +1,11 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
-import { Product } from "../../models/product";
 
+import { Link } from 'react-router-dom';
+import { Product } from "../../models/product";
 
 interface Props {
     product: Product;
 }
-
 export default function ProductCard({ product }: Props) {
     return (
         <Card>
@@ -35,7 +35,7 @@ export default function ProductCard({ product }: Props) {
             </CardContent>
             <CardActions>
                 <Button size="small">Add to Cart</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,7 @@
-
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Header from './Header';
 import { useState } from 'react';
-import Catalog from '../features/catalog/Catalog';
+import { Outlet } from 'react-router-dom';
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -15,20 +14,17 @@ function App() {
             }
         }
     })
-
     function handleThemeChange() {
         setDarkMode(!darkMode);
     }
-
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Catalog />
+                <Outlet />
             </Container>
         </ThemeProvider>
     );
 }
-
 export default App
